caba1240836d296ef76106547d817655f532871f
njuFerret <emagazi.cn@gmail.com>
2024-10-19 15:01:22 +0800

    1.6.1-1

--- build_clang.bat
+++ build_clang.bat
@@ -24,7 +24,7 @@ set _llvm_tag_ver=llvmorg-%_llvm_ver%
 set LLVM_DIR=%BUILD_START_DIR%\llvm-project
 @REM set CLAZY_SRC=%BUILD_START_DIR%\clazy
 
-@REM set QT_BASE_DIR=%INSTALL_PREFIX%\Qt
+set QT_BASE_DIR=%INSTALL_PREFIX%\Qt
 @REM set QT_INSTALL_DIR=%QT_BASE_DIR%\%_qt_ver%\qt
 set CLANG_INSTALL_DIR=%INSTALL_PREFIX%\libclang
 
@@ -45,29 +45,30 @@ curl -L -o ninja-win.zip https://github.com/ninja-build/ninja/releases/download/
 @REM curl -L -o "%_pkgfn%.tar.xz" "https://download.qt.io/official_releases/qt/%_qt_major_ver%/%_qtver%/single/%_pkgfn%.tar.xz"
 curl -L -o strawberry-perl.zip https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_54001_64bit_UCRT/strawberry-perl-5.40.0.1-64bit-portable.zip
 @REM curl -L -o openssl-3.tar.gz "https://github.com/openssl/openssl/releases/download/openssl-3.3.2/openssl-3.3.2.tar.gz"
-@REM curl -L -o MingW.7z https://github.com/niXman/mingw-builds-binaries/releases/download/14.2.0-rt_v12-rev0/x86_64-14.2.0-release-posix-seh-ucrt-rt_v12-rev0.7z
+curl -L -o MingW.7z https://github.com/niXman/mingw-builds-binaries/releases/download/14.2.0-rt_v12-rev0/x86_64-14.2.0-release-posix-seh-ucrt-rt_v12-rev0.7z
 
 curl -L -o openssl.7z https://github.com/njuFerret/qt6_mingw_build/releases/download/deps/openssl_3.3.2_mingw-x86_64-13.1.7z
-curl -L -o MinGW.7z https://github.com/njuFerret/qt6_mingw_build/releases/download/deps/%mingw%.7z
+@REM curl -L -o MinGW.7z https://github.com/njuFerret/qt6_mingw_build/releases/download/deps/%mingw%.7z
 
-
-@REM cd \
-@REM mkdir Dev
-@REM cd Dev
-@REM echo 当前路径：%CD%
-
-@REM python -V
-@REM g++ -v
 
 @REM cmake解压后根目录为文件名, 类似 cmake-3.30.5-windows-x86_64
 @REM ninja-win为单文件, 需要创建文件夹, 这里创建ninja文件夹
 @REM MinGW解压后根目录为 mingw64 
 
+echo **********************    解压7zip    ****************************
 7zr x 7zip.exe -o7zip
+echo **********************    解压cmake   ****************************
 7z x cmake.zip
+echo **********************    解压ninja   ****************************
 7z x ninja-win.zip -oninja
+echo **********************    解压MingW   ****************************
 7z x MingW.7z
+if exist %ROOT%\mingw64 (
+    move %ROOT%\mingw64 %ROOT%\MinGW
+)
+echo **********************    解压openssl ****************************
 7z x openssl.7z -o%QT_BASE_DIR%
+echo **********************    解压完成    ****************************
 
 @REM echo %PATH%
 @REM 7z x "%_pkgfn%.tar.xz"
